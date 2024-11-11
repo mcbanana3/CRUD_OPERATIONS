5320056df30bfb98d92da1e7c605a4fe42aab13f
Aryan Yalavarthi <2300030049@kluniversity.in>
2024-11-11 23:26:55 +0530

    Changes to the Backend

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const studentRoutes = require('./routes/studentRoutes');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://crud-operations-frontend-lmlv.onrender.com'
+}));
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
